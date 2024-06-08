abd9de04b5269808ff70c73ef15ac1c72e6a7433
MedElba <medelbayoumi@gmail.com>
2024-06-08 15:52:46 +0200

    typescript mastery

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -45,7 +45,7 @@ const Navbar: React.FC = () => {
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex ">
             Ahmad &nbsp;
-            <span className="sm:block hidden"> | JavaScript Mastery</span>
+            <span className="sm:block hidden"> | Typescript Mastery</span>
           </p>
         </Link>
 
